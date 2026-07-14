0975b7be7cac440a2b9392a6e416b07ccbc333ed
nikoloz-code23 <73937823+nikoloz-code23@users.noreply.github.com>
2026-07-15 02:16:55 +0400

    Modify the app.js file to do the following: - Import the task template javascript file. - Import the utility functions to the app.js. - Add an event listener to the document on the DOMContentLoaded, to make sure that the code only runs when the content loads. - Add an event listener on the New Task Button so that it shows the modal background. - Add an event listener to the tasks wrapper element on the change event and have it contain an event delegation for the status changes. TODO: - Add a form to the modal, so that you can add new elements.

---
+++ assets/js/app.js
@@ -0,0 +1,42 @@
+import { taskTemplate } from "./templates.js";
+import {
+  createTaskObject,
+  saveToLocalStorage,
+  getDataFromLocalStorage,
+  getDataFetch,
+} from "./utils.js";
+
+document.addEventListener("DOMContentLoaded", async (e) => {
+  const mainElement = document.querySelector("main");
+  const newTaskButton = mainElement.querySelector("#new-task-button");
+  const modalElement = mainElement.querySelector("#new-task-modal");
+  const tasksWrapperElement = mainElement.querySelector(".tasks-wrapper");
+
+  newTaskButton.addEventListener("click", () => {
+    modalElement.classList.toggle("not-visible");
+    /*
+    tasksWrapperElement.insertAdjacentHTML(
+      "beforeend",
+      taskTemplate({
+        id: 2,
+        title: "hey",
+        completed: false,
+      }),
+    );
+    */
+  });
+
+  tasksWrapperElement.addEventListener("change", (e) => {
+    const element = e.target;
+
+    if (element.classList.contains("status-select")) {
+      if (element.value === "not-completed") {
+        element.classList.remove("status-completed");
+        element.classList.add("status-not-completed");
+        return;
+      }
+      element.classList.remove("status-not-completed");
+      element.classList.add("status-completed");
+    }
+  });
+});
