652fd660c1daadf627e61ac784400771b7ab9a15
nikoloz-code23 <73937823+nikoloz-code23@users.noreply.github.com>
2026-07-16 20:45:19 +0400

    Modify the renderTasks function to filter properly: - Add taskName and taskStatus predicate functions. - If filterObject exists, filter the tasks data. - After filtering, render it and display it in the DOM.

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,28 +1,38 @@
-import { createTaskObject } from "./utils.js";
+import { createTaskObject, stringIsEmpty } from "./utils.js";
 import { taskTemplate } from "./templates.js";
+
+const taskNamePredicate = (taskTitle, compareAgainst) => {
+  return taskTitle.toLocaleLowerCase().includes(compareAgainst);
+};
+
+const taskStatusPredicate = (taskStatus, compareAgainst) => {
+  return taskStatus.toLocaleLowerCase() === compareAgainst;
+};
 
 export function renderTasks(tasks, wrapperElement, filterObject) {
   let tasksToRender = tasks;
   wrapperElement.innerHTML = "";
+
   if (filterObject) {
-    // The priority should be the name of the task.
-    // This is a quick hack, but change if needed.
-    if (filterObject.taskName) {
-      tasksToRender = tasks.filter(
-        (element) => element.title === filterObject.taskName,
-      );
-    } else if (filterObject.taskStatus) {
-      tasksToRender = tasks.filter(
-        (element) => element.completed === filterObject.taskStatus,
-      );
-    } else if (filterObject.taskName && filterObject.taskStatus) {
-      tasksToRender = tasks.filter(
-        (element) =>
-          element.title === filterObject.taskName &&
-          element.completed === filterObject.taskStatus,
-      );
+    const { taskName, taskStatus } = filterObject;
+    const hasNameFilter = !stringIsEmpty(taskName);
+    const hasStatusFilter = !stringIsEmpty(taskStatus) && taskStatus !== "all";
+
+    if (hasNameFilter || hasStatusFilter) {
+      // De morgan's law.
+      tasksToRender = tasksToRender.filter((task) => {
+        // if hasNameFilter isn't empty (true)
+        // hasNameFilter here will be false and activate the predicate.
+        const matchesName =
+          !hasNameFilter || taskNamePredicate(task.title, taskName);
+        const matchesStatus =
+          !hasStatusFilter || taskStatusPredicate(task.completed, taskStatus);
+
+        return matchesName && matchesStatus;
+      });
     }
   }
+
   for (const element of tasksToRender) {
     const newTask = createTaskObject(
       element.id,
